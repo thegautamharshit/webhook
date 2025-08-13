e5ecbab62fc1fb5502b29101ae9d60581bf2e5b2
Harshit Gautam <129443335+thegautamharshit@users.noreply.github.com>
2025-08-14 00:27:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // Set port and verify_token
 const port = process.env.PORT || 3000;
-const verifyToken = webhook_verification_token;
+const verifyToken = "my_secure_token_2025";
 
 // Route for GET requests
 app.get('/', (req, res) => {
